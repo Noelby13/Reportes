e92d40bb73ce868370c668c078096fca98573bde
Noelby13 <hnoelreyes25@gmail.com>
2023-03-29 08:45:53 -0600

    Aplicación que implementa RDCL con VB.NET

---
+++ Form1.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Data.SqlClient
+Imports Microsoft.Reporting.WinForms
+
+Public Class Form1
+
+    Sub verReporte(ByVal t As DataTable, ByVal ds As String, ByVal nombreRpt As String)
+        Try
+            Dim rpt As New ReportDataSource(ds, t)
+            FrmVistaPrevia.ReportViewer1.LocalReport.DataSources.Clear()
+            FrmVistaPrevia.ReportViewer1.LocalReport.DataSources.Add(rpt)
+            FrmVistaPrevia.ReportViewer1.LocalReport.ReportPath = nombreRpt
+            FrmVistaPrevia.ReportViewer1.Refresh()
+            FrmVistaPrevia.Show()
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al mostrar el reporte")
+        End Try
+    End Sub
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Btn.Click
+        Try
+            Dim tsql As String = "Select* from jobs"
+            Dim conex As New SqlConnection(My.Settings.cStrDBRH)
+            Dim da As New SqlDataAdapter(tsql, conex)
+            Dim t As New DataTable
+            da.Fill(t)
+            verReporte(t, "dsRegistros", "diseñosRpt\rptCargo.rdlc")
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al cargar el reporte")
+        End Try
+    End Sub
+
+End Class
